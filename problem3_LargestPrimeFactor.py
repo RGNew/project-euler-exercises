2eb05be28ba813b849a31ee837d0ccb6e0f43923
RGNew <rn1076@gmail.com>
2018-12-04 21:50:10 -0600

    Changed naming convention and fixing power issue.

--- problem3_LargestPrimeFactor.py
+++ problem3_LargestPrimeFactor.py
@@ -1,16 +1,16 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-number_to_check = 600851475143
-factorial = 2
-last_fact = 1
+number_to_check = 3430
+factor = 2
+last_factor = 1
 
 while (number_to_check > 1):
 
-    if (number_to_check % factorial == 0):
-        last_fact = factorial
-        number_to_check /= factorial
+    if (number_to_check % factor == 0):
+        last_factor = factor
+        number_to_check /= factor
+    else:
+        factor += 1
 
-    factorial += 1
-
-print(last_fact)
+print(last_factor)
